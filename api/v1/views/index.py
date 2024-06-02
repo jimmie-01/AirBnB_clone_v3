7fe0d67c6508dae507bd99db9ed715e20ca5d918
Fakile Razaq <razaqjimmie91@gmail.com>
2024-06-02 22:50:07 +0100

    RESTful API Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,29 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def api_status():
 	"""
-	
+	Returns a JSON response for RESTFUL API health
 	"""
 	response = {'status': "OK"}
 	return jsonify(response)
+
+
+@app_views.route('/stats')
+def get_stats():
+	"""
+	stats of all objs route
+	"""
+	stats = {
+		'amenities': storage.count('Amenity'),
+		'cities': storage.count('City'),
+		'places': storage.count('Place'),
+		'reviews': storage.count('Review'),
+		'states': storage.count('State'),
+		'users': storage.count('User'),
+    }
+	return jsonify(stats)
