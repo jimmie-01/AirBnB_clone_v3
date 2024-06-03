f992ae5349c298480a06a904cf74fb001675ab48
Fakile Razaq <razaqjimmie91@gmail.com>
2024-06-03 09:19:54 +0100

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Flask Application 
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,8 +11,24 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+	"""
+	Teardown Function
+	"""
+	storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+	"""
+	handles 404 error
+	"""
+	response = ("error": "Not Found")
+	return jsonify(response), 404
+
 
 if __name__ == "__main__":
 	HOST = getenv('HBNB_API_HOST', '0.0.0.0')
 	PORT = int(getenv('HBNB_API_PORT', 5000))
-	app.run(host=HOST, port=PORT, threaded=True)
+	app.run(debug=True, host=HOST, port=PORT, threaded=True)
