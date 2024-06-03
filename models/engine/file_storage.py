17d923ef9ce0a274b2838d6fa5dc5068f6f2c7c1
Fakile Razaq <razaqjimmie91@gmail.com>
2024-06-03 09:37:44 +0100

    Updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -74,27 +74,27 @@ class FileStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
-        if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-        return None
+        if cls and id:
+            if cls in classes.values() and isinstance(id, str):
+                all_objects = self.all(cls)
+                for key, value in all_objects.items():
+                    if key.split('.')[1] == id:
+                        return value
+            else:
+                return
+        return
 
     def count(self, cls=None):
         """
         count the number of objects in storage
         """
-        all_class = classes.values()
-
         if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
+            inst_of_all_cls = self.all()
+            return len(inst_of_all_cls)
+            for clas,value in classes.items():
+                if cls == clas or cls == value:
+                    all_inst_of_prov_cls = self.all(cls)
+                    return len(all_inst_of_prov_cls)
+            if cls not in classes.values():
+                return
 
-        return count
